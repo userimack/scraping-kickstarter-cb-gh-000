c364e2eeb5b4bf20142a73a196142a925a20d340
Mahendra Yadav <mahendra.k12@gmail.com>
2018-05-12 18:02:07 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -9,7 +9,8 @@ def create_project_hash
   kickstarter = Nokogiri::HTML(html)
   
   kickstarter.css("li.project.grid_4").each do |project| 
-    projects[project] = {}
+    title = project.css("h2.bbcard_name strong a").text
+    projects[title.to_sym] = {}
   end 
   
   projects 
@@ -18,7 +19,7 @@ end
 pry.binding 
 create_project_hash
 # projects: kickstarter.css("li.project.grid_4")
-# title: project.css("h2.bbcard_name strong a").text
+# title: 
 #image_link: project.css("div.project-thumbnail a img").attribute("src").value
 # description: project.css("p.bbcard_blurb").text
 # location: project.css("ul.project-meta li a span.location-name").text
