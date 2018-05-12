77951f0ce704f39a339e86e903a42109bd592c83
Mahendra Yadav <mahendra.k12@gmail.com>
2018-05-12 18:03:16 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -20,13 +20,3 @@ def create_project_hash
  
   projects
 end
-
-#pry.binding 
-#create_project_hash
-# projects: kickstarter.css("li.project.grid_4")
-# title: 
-#image_link: project.css("div.project-thumbnail a img").attribute("src").value
-# description: project.css("p.bbcard_blurb").text
-# location: project.css("ul.project-meta li a span.location-name").text
-
-#funded: project.css("ul.project-stats li.first.funded strong").text.gsub("%", "").to_i 
