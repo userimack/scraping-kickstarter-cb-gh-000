833da13f5cd1fa3ff67c0181b5f47cbe3be08034
Mahendra Yadav <mahendra.k12@gmail.com>
2018-05-12 17:42:16 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,4 +1,9 @@
 # require libraries/modules here
+require "nokogiri"
+
+html = File.read('fiztures/kickstarter.html')
+
+kickstarter = Nokogiri::HTML(html)
 
 def create_project_hash
   # write your code here
